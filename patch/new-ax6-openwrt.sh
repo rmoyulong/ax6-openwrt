43416338e856fa9cf7114e2536bcaaf4115f1caa
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-03-05 14:27:35 +0800

    Delete patch/new-ax6-openwrt.sh

--- patch/new-ax6-openwrt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd openwrt
-
-mv $GITHUB_WORKSPACE/patch/0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch 0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch
-git apply 0001-ipq807x-add-stock-layout-variant-for-redmi-ax6.patch
