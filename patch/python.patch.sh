f1396ab66c1f179f72a4e8d0c58de5c9a6cc7821
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-04-29 22:42:05 +0800

    修改homeproxy初始设置+old python

--- patch/python.patch.sh
+++ patch/python.patch.sh
@@ -5,3 +5,14 @@ cd openwrt
 rm -rf feeds/packages/lang/python
 
 merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
+#================修改homeproxy初始设置===========================
+git clone https://github.com/rmoyulong/Lite_OpenWrt Lite_OpenWrt 
+
+#如果files文件夹不存在，创建文件夹
+if [ ! -d "./files" ]; then
+  mkdir ./files
+fi
+
+cp -rf Lite_OpenWrt/homeproxy/files/* ./files
+rm -rf Lite_OpenWrt
