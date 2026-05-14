4c4a7e803d5b3eeb2bb41378182a18cc0123efd0
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-14 15:23:53 +0800

    Add files via upload

--- patch/python.patch.sh
+++ patch/python.patch.sh
@@ -5,7 +5,3 @@ cd openwrt
 rm -rf feeds/packages/lang/python
 
 merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
-
-#替换回qualcommax6.12
-unzip $GITHUB_WORKSPACE/patch/qualcommax.zip
-cat feeds.conf.default
