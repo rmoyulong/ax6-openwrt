50f8bc318ef9b1198a7e07fae53dd229d4947b5e
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-14 15:09:36 +0800

    把qualcommax6.12支持添加回去

--- patch/python.patch.sh
+++ patch/python.patch.sh
@@ -5,3 +5,6 @@ cd openwrt
 rm -rf feeds/packages/lang/python
 
 merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages feeds/packages/lang lang/python
+
+#替换回qualcommax6.12
+unzip $GITHUB_WORKSPACE/patch/qualcommax.zip
